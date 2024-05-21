ec117afc3fc4fbd4dfee3ab762f3fabce9d1d2e0
ALIGULU43 <ld801dw9n@code.edu.az>
2024-05-21 22:46:16 +0400

    task_react1

---
+++ src/component/Shob.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div className="shob">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-8 col-md-12"><img src="https://preview.colorlib.com/theme/shoppers/images/hero_1.jpg" alt="" /></div>
+                <div className="col-lg-4 col-md-12 mt-4"><h1>Finding Your Perfect Shoes</h1><p className='mb-4 mt-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at iaculis quam. Integer accumsan tincidunt fringilla.</p>
+                <button className='btn1'>SHOP NOW</button></div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Shop
